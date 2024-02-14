82dcf8b70b2e021cc9afdf5fda54d502ce2dd44b
Nick Dumas <drakonik@gmail.com>
2024-02-14 14:06:38 -0500

    refining the debug statements

--- resolver.go
+++ resolver.go
@@ -11,7 +11,10 @@ type Resolver struct {
 
 func (r *Resolver) ResolveWikilink(n *wikilink.Node) ([]byte, error) {
 	out := make([]byte, 0)
-	log.Printf("Resolving node: %#+v\n", n)
+	log.Printf("%#v\n", map[string][]byte{
+		"Target":   n.Target,
+		"Fragment": n.Fragment,
+	})
 
 	return out, nil
 }
