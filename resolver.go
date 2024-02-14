4b6236a2e786cafed7528f9e565a7046eeaf985b
Nick Dumas <drakonik@gmail.com>
2024-02-14 13:55:01 -0500

    now the resolver will actually do something

--- resolver.go
+++ resolver.go
@@ -1,10 +1,17 @@
 package resolver
 
 import (
+	"log"
+
 	"go.abhg.dev/goldmark/wikilink"
 )
 
 type Resolver struct {
 }
 
-func (r *Resolver) ResolveWikilink(n *wikilink.Node) ([]byte, error)
+func (r *Resolver) ResolveWikilink(n *wikilink.Node) ([]byte, error) {
+	out := make([]byte, 0)
+	log.Printf("Resolving node: %#+v\n", n)
+
+	return out, nil
+}
